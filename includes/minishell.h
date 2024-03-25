f2d4f0fbb51e61a5e4d1ed846060bd6be05f8d9b
Olimpiya Obreshkova <ollydimitrova94@gmail.com>
2024-03-25 16:30:55 +0100

    h

--- includes/minishell.h
+++ includes/minishell.h
@@ -10,4 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
+# include "../libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdbool.h>
+
+#endif
