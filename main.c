c86f766a204df499fe885471ba2fdb03b0ae2814
Jasmin Ngoc Han Nguyen <jasminnguyen001@web.de>
2024-03-25 17:51:53 +0100

    started working on the prompt

--- main.c
+++ main.c
@@ -6,7 +6,18 @@
 /*   By: jasnguye <jasnguye@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 14:39:02 by jasnguye          #+#    #+#             */
-/*   Updated: 2024/03/25 14:39:05 by jasnguye         ###   ########.fr       */
+/*   Updated: 2024/03/25 17:49:20 by jasnguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Libft/libft.h"
+#include "includes/minishell.h"
+
+int main(int argc, char *argv[])
+{
+	char *read_line;
+
+	read_line = readline("minishell: ");
+	printf("%s\n", read_line);
+	return(0);
+}
