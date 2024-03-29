c253924a912e621ef06ffd9be1018fe7af56c8d3
Jasmin Ngoc Han Nguyen <jasminnguyen001@web.de>
2024-03-29 12:29:01 +0100

    started working on expansion

--- expansion.c
+++ expansion.c
@@ -6,14 +6,38 @@
 /*   By: jasnguye <jasnguye@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 17:27:10 by jasnguye          #+#    #+#             */
-/*   Updated: 2024/03/28 17:46:31 by jasnguye         ###   ########.fr       */
+/*   Updated: 2024/03/29 12:25:36 by jasnguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Libft/libft.h"
 #include "includes/minishell.h"
 
-void expansion(t_data *data)
+char *get_value(char *variable_name)
+{
+	return getenv(variable_name);
+}
+void replace_variable(t_token *token)
+{
+	char *variable_name = token->value + 1;
+	char *value = get_value(variable_name);
+	// size_t i = 0;
+	if(!value)
+	{
+		ft_printf("environment variable doesn't exist\n");
+	}
+	else
+	{/* 
+		while(variable_name[i] != '\0')
+		{
+			i++;
+		} */
+		ft_strlcpy(variable_name, value, ft_strlen(value));
+	}
+	
+	
+}
+void expansion(t_data *data) //for now: only works if the $ is at the beginning of the variable
 {
 	t_token *current = data->token_list;
 	int i;
@@ -22,8 +46,7 @@ void expansion(t_data *data)
 		i = 0;
 		if(current->value[i] == '$')
 		{
-			i++;
-			replace_variable(data, current->value);
+			replace_variable(current);
 		}
 		current = current->next;
 	}
