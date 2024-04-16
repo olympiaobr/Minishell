bb933c940e5e272af5cc55c5cf40bc09c7dd8f18
Olimpiya Obreshkova <ollydimitrova94@gmail.com>
2024-04-16 13:06:03 +0200

    changed h

--- Libft/libft.h
+++ Libft/libft.h
@@ -62,7 +62,7 @@ char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
+int		ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putchar(char c);
 
