a3534c09ffc1ced577b42f5be8b7f6fb7a0099c2
Olimpiya Obreshkova <ollydimitrova94@gmail.com>
2024-05-10 18:21:25 +0200

    added echo -nnnnnngsdn feature

--- path.c
+++ path.c
@@ -59,20 +59,6 @@ int init_env(t_data *data, char **env)
 }
 
 
-void append_slash(char **directory)
-{
-    char *temp;
-
-    temp = *directory;
-    *directory = ft_strjoin(temp, "/");
-    if (!(*directory))
-	{
-        perror("Error appending slash to PATH directory");
-        free(temp);
-        exit(EXIT_FAILURE);
-    }
-    free(temp);
-}
 void prepare_environment(t_data *data)
 {
     int i;
@@ -96,54 +82,6 @@ void prepare_environment(t_data *data)
         append_slash(&(data->path_dirs[i]));
         i++;
     }
-}
-int match_found(const char *haystack, const char *needle)
-{
-    while (*haystack && *needle && *haystack == *needle)
-    {
-        haystack++;
-        needle++;
-    }
-    return (*needle == '\0');
-}
-
-char *cust_strstr(const char *haystack, const char *needle)
-{
-    if (*needle == '\0')
-    {
-        return ((char *)haystack);
-    }
-
-    while (*haystack != '\0')
-    {
-        if (*haystack == *needle)
-        {
-            if (match_found(haystack, needle))
-            {
-                return ((char *)haystack);
-            }
-        }
-        haystack++;
-    }
-    return (NULL);
-}
-
-char *cust_getenv(const char *name, t_data *data)
-{
-    int i = 0;
-    char *env_entry;
-    char *found;
-
-    while ((env_entry = data->env[i]) != NULL)
-    {
-        found = cust_strstr(env_entry, name);
-        if (found && found[ft_strlen(name)] == '=' && (found == env_entry || *(found - 1) == '\0'))
-        {
-            return (found + ft_strlen(name) + 1);
-        }
-        i++;
-    }
-    return (NULL);
 }
 
 void initialize_shell_components(t_data *data)
--- utils2.c
+++ utils2.c
@@ -17,3 +17,68 @@ void ft_error(char *err)
 {
     ft_putstr_fd(err, 2);
 }
+void append_slash(char **directory)
+{
+    char *temp;
+
+    temp = *directory;
+    *directory = ft_strjoin(temp, "/");
+    if (!(*directory))
+	{
+        perror("Error appending slash to PATH directory");
+        free(temp);
+        exit(EXIT_FAILURE);
+    }
+    free(temp);
+}
+
+int match_found(const char *haystack, const char *needle)
+{
+    while (*haystack && *needle && *haystack == *needle)
+    {
+        haystack++;
+        needle++;
+    }
+    return (*needle == '\0');
+}
+
+char *cust_strstr(const char *haystack, const char *needle)
+{
+    if (*needle == '\0')
+    {
+        return ((char *)haystack);
+    }
+
+    while (*haystack != '\0')
+    {
+        if (*haystack == *needle)
+        {
+            if (match_found(haystack, needle))
+            {
+                return ((char *)haystack);
+            }
+        }
+        haystack++;
+    }
+    return (NULL);
+}
+
+char *cust_getenv(const char *name, t_data *data)
+{
+    int i = 0;
+    char *env_entry;
+    char *found;
+
+    while ((env_entry = data->env[i]) != NULL)
+    {
+        found = cust_strstr(env_entry, name);
+        if (found && found[ft_strlen(name)] == '=' && (found == env_entry || *(found - 1) == '\0'))
+        {
+            return (found + ft_strlen(name) + 1);
+        }
+        i++;
+    }
+    return (NULL);
+}
+
+
