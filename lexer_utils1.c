01a7f4066619535d0340f963f031c856bd3f59b6
Olimpiya Obreshkova <olobresh@c3a5c2.42berlin.de>
2024-05-11 21:18:20 +0200

    norminetting

--- lexer_utils1.c
+++ lexer_utils1.c
@@ -18,49 +18,49 @@ int	whitespace_chars(char c)
 		|| c == '\r');
 }
 
-t_token *init_token(token_type type, char *value, int is_quoted)
+t_token	*init_token(token_type type, char *value, int is_quoted)
 {
-    t_token *t;
+	t_token	*t;
 
-    t = malloc(sizeof(t_token));
-    if (!t)
-        return NULL;
-    t->type = type;
-    if (value)
-    {
-        t->value = ft_strdup(value);
-    }
-    else
-    {
-        t->value = NULL;
-    }
-    t->is_quoted = is_quoted;
-    t->next = NULL;
-    return (t);
+	t = malloc(sizeof(t_token));
+	if (!t)
+		return (NULL);
+	t->type = type;
+	if (value)
+	{
+		t->value = ft_strdup(value);
+	}
+	else
+	{
+		t->value = NULL;
+	}
+	t->is_quoted = is_quoted;
+	t->next = NULL;
+	return (t);
 }
 
 t_token	*allocate_token(token_type type, char *val, int is_quoted)
 {
-    char	*trimmed_val;
-    t_token	*token;
+	char	*trimmed_val;
+	t_token	*token;
 
-    if (is_quoted)
+	if (is_quoted)
 	{
-        trimmed_val = ft_strdup(val);
-    }
+		trimmed_val = ft_strdup(val);
+	}
 	else
 	{
-        trimmed_val = trim_value(val);
-    }
-    if (!trimmed_val)
+		trimmed_val = trim_value(val);
+	}
+	if (!trimmed_val)
 	{
-        return (NULL);
-    }
-    token = init_token(type, trimmed_val, is_quoted);
-    free(trimmed_val);
-    if (token == NULL)
-        return (NULL);
-    return (token);
+		return (NULL);
+	}
+	token = init_token(type, trimmed_val, is_quoted);
+	free(trimmed_val);
+	if (token == NULL)
+		return (NULL);
+	return (token);
 }
 
 void	append_token(t_token **token_list, t_token *new_token)
@@ -68,11 +68,11 @@ void	append_token(t_token **token_list, t_token *new_token)
 	t_token	*current;
 
 	if (!token_list || !new_token)
-    {
-        free(new_token->value);
-        free(new_token);
-        return;
-    }
+	{
+		free(new_token->value);
+		free(new_token);
+		return ;
+	}
 	if (!*token_list)
 	{
 		*token_list = new_token;
