18b23254e9ab58baec4e4e7c2aab37ab6b7202d0
Olimpiya Obreshkova <olobresh@c3a5c2.42berlin.de>
2024-05-12 18:42:15 +0200

    norminetting

--- lexer_utils6.c
+++ lexer_utils6.c
@@ -13,52 +13,52 @@
 #include "Libft/libft.h"
 #include "includes/minishell.h"
 
-void update_token_type_before_processing(t_data *data)
+void	update_token_type_before_processing(t_data *data)
 {
-    if (data->expect_command)  // or if (global_expect_command)
-    {
-        data->type = T_COMMAND;  // or global_type = T_COMMAND;
-        data->expect_command = 0;  // or global_expect_command = 0;
-    }
-    else
-    {
-        data->type = T_ARGUMENT;  // or global_type = T_ARGUMENT;
-    }
+	if (data->expect_command)
+	{
+		data->type = T_COMMAND;
+		data->expect_command = 0;
+	}
+	else
+	{
+		data->type = T_ARGUMENT;
+	}
 }
 
-void process_based_on_type(t_data *data, char *str, size_t *idx)
+void	process_based_on_type(t_data *data, char *str, size_t *idx)
 {
-    if (shell_operators(str[*idx]))
-    {
-        handle_operator(data, str, idx);
-        update_expectations(data);
-    }
-    else
-    {
-        update_token_type_before_processing(data);
-        if (process_chars(data, str, idx, data->type) != 0)
-        {
-            ft_printf("Failed to tokenize input at index %zu\n", *idx);
-            free_tokens(data);
-            return;
-        }
-    }
+	if (shell_operators(str[*idx]))
+	{
+		handle_operator(data, str, idx);
+		update_expectations(data);
+	}
+	else
+	{
+		update_token_type_before_processing(data);
+		if (process_chars(data, str, idx, data->type) != 0)
+		{
+			ft_printf("Failed to tokenize input at index %zu\n", *idx);
+			free_tokens(data);
+			return ;
+		}
+	}
 }
 
-
-void process_input(t_data *data, char *str)
+void	process_input(t_data *data, char *str)
 {
-    size_t idx = 0;
-    data->expect_command = 1;
-    data->type = T_COMMAND;
+	size_t	idx;
 
-    while (str[idx])
-    {
-        if (whitespace_chars(str[idx]))
-        {
-            idx++;
-            continue;
-        }
-        process_based_on_type(data, str, &idx);
-    }
+	idx = 0;
+	data->expect_command = 1;
+	data->type = T_COMMAND;
+	while (str[idx])
+	{
+		if (whitespace_chars(str[idx]))
+		{
+			idx++;
+			continue ;
+		}
+		process_based_on_type(data, str, &idx);
+	}
 }
